ef2102d36e284ad3e0919cf82cf949cb408075f6
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-04-10 17:07:02 +0200

    tttt

--- app/Database/Migrations/2026-04-10-145929_Categories.php
+++ app/Database/Migrations/2026-04-10-145929_Categories.php
@@ -6,13 +6,27 @@ use CodeIgniter\Database\Migration;
 
 class Categories extends Migration
 {
-    public function up()
+    public function up(): void
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'libelle' => [
+                'type'       => 'VARCHAR',
+                'constraint' => 100,
+                'null'       => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('categories');
     }
 
-    public function down()
+    public function down(): void
     {
-        //
+        $this->forge->dropTable('categories');
     }
 }
--- app/Database/Seeds/CategorieSeeder.php
+++ app/Database/Seeds/CategorieSeeder.php
@@ -6,8 +6,15 @@ use CodeIgniter\Database\Seeder;
 
 class CategorieSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
-        //
+        $data = [
+            ['id' => 1, 'libelle' => 'Animés'],
+            ['id' => 2, 'libelle' => 'Films'],
+            ['id' => 3, 'libelle' => 'Séries'],
+        ];
+
+        // Utilisation de insertBatch car on insère plusieurs lignes d'un coup
+        $this->db->table('categories')->insertBatch($data);
     }
 }
