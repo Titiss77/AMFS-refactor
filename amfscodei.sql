276727a93a59fb32d88fa6a93e3337428825077f
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-05-16 17:36:54 +0200

    db good

--- amfscodei.sql
+++ amfscodei.sql
@@ -1,12 +1,15 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
+
 DROP TABLE IF EXISTS `auth_groups_users`;
 CREATE TABLE `auth_groups_users` (
   `id` int UNSIGNED NOT NULL,
   `user_id` int UNSIGNED NOT NULL,
   `group` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` datetime NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `auth_identities`;
 CREATE TABLE `auth_identities` (
   `id` int UNSIGNED NOT NULL,
@@ -21,7 +24,8 @@ CREATE TABLE `auth_identities` (
   `last_used_at` datetime DEFAULT NULL,
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `auth_logins`;
 CREATE TABLE `auth_logins` (
   `id` int UNSIGNED NOT NULL,
@@ -32,14 +36,16 @@ CREATE TABLE `auth_logins` (
   `user_id` int UNSIGNED DEFAULT NULL,
   `date` datetime NOT NULL,
   `success` tinyint(1) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `auth_permissions_users`;
 CREATE TABLE `auth_permissions_users` (
   `id` int UNSIGNED NOT NULL,
   `user_id` int UNSIGNED NOT NULL,
   `permission` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `created_at` datetime NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `auth_remember_tokens`;
 CREATE TABLE `auth_remember_tokens` (
   `id` int UNSIGNED NOT NULL,
@@ -49,7 +55,8 @@ CREATE TABLE `auth_remember_tokens` (
   `expires` datetime NOT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `auth_token_logins`;
 CREATE TABLE `auth_token_logins` (
   `id` int UNSIGNED NOT NULL,
@@ -60,18 +67,21 @@ CREATE TABLE `auth_token_logins` (
   `user_id` int UNSIGNED DEFAULT NULL,
   `date` datetime NOT NULL,
   `success` tinyint(1) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `division`;
 CREATE TABLE `division` (
   `id` int UNSIGNED NOT NULL,
   `id_header` int UNSIGNED NOT NULL,
   `nom` varchar(50) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 DROP TABLE IF EXISTS `header`;
 CREATE TABLE `header` (
   `id` int UNSIGNED NOT NULL,
   `nom` varchar(50) NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 DROP TABLE IF EXISTS `item`;
 CREATE TABLE `item` (
   `id` int UNSIGNED NOT NULL,
@@ -86,8 +96,10 @@ CREATE TABLE `item` (
   `episode` varchar(10) DEFAULT NULL,
   `saison` int DEFAULT NULL,
   `position` int DEFAULT '0',
-  `date_sortie` datetime DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+  `date_sortie` datetime DEFAULT NULL,
+  `deleted_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
 DROP TABLE IF EXISTS `migrations`;
 CREATE TABLE `migrations` (
   `id` bigint UNSIGNED NOT NULL,
@@ -97,7 +109,8 @@ CREATE TABLE `migrations` (
   `namespace` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
   `time` int NOT NULL,
   `batch` int UNSIGNED NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `settings`;
 CREATE TABLE `settings` (
   `id` int NOT NULL,
@@ -108,7 +121,8 @@ CREATE TABLE `settings` (
   `context` varchar(255) COLLATE utf8mb4_general_ci DEFAULT NULL,
   `created_at` datetime NOT NULL,
   `updated_at` datetime NOT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int UNSIGNED NOT NULL,
@@ -120,62 +134,86 @@ CREATE TABLE `users` (
   `created_at` datetime DEFAULT NULL,
   `updated_at` datetime DEFAULT NULL,
   `deleted_at` datetime DEFAULT NULL
-) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
 ALTER TABLE `auth_groups_users`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `auth_groups_users_user_id_foreign` (`user_id`);
+
 ALTER TABLE `auth_identities`
-ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `type_secret` (`type`, `secret`),
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `type_secret` (`type`,`secret`),
   ADD KEY `user_id` (`user_id`);
+
 ALTER TABLE `auth_logins`
-ADD PRIMARY KEY (`id`),
-  ADD KEY `id_type_identifier` (`id_type`, `identifier`),
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_type_identifier` (`id_type`,`identifier`),
   ADD KEY `user_id` (`user_id`);
+
 ALTER TABLE `auth_permissions_users`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `auth_permissions_users_user_id_foreign` (`user_id`);
+
 ALTER TABLE `auth_remember_tokens`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `selector` (`selector`),
   ADD KEY `auth_remember_tokens_user_id_foreign` (`user_id`);
+
 ALTER TABLE `auth_token_logins`
-ADD PRIMARY KEY (`id`),
-  ADD KEY `id_type_identifier` (`id_type`, `identifier`),
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_type_identifier` (`id_type`,`identifier`),
   ADD KEY `user_id` (`user_id`);
+
 ALTER TABLE `division`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD KEY `id_header` (`id_header`);
+
 ALTER TABLE `header`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `item`
-ADD PRIMARY KEY (`id`),
-  ADD KEY `id_user` (`id_user`, `id_division`),
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`,`id_division`),
   ADD KEY `id_division` (`id_division`);
+
 ALTER TABLE `migrations`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `settings`
-ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`);
+
 ALTER TABLE `users`
-ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`);
+
+
 ALTER TABLE `auth_groups_users`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `auth_identities`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `auth_logins`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `auth_permissions_users`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `auth_remember_tokens`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `division`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
 ALTER TABLE `item`
-MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+
 ALTER TABLE `division`
-ADD CONSTRAINT `division_ibfk_1` FOREIGN KEY (`id_header`) REFERENCES `header` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+  ADD CONSTRAINT `division_ibfk_1` FOREIGN KEY (`id_header`) REFERENCES `header` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
 ALTER TABLE `item`
-ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `item_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `item_ibfk_2` FOREIGN KEY (`id_division`) REFERENCES `division` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
