2a2c2cf13b884f3b5bcb7d4794b5f38d611770a7
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-07-02 20:31:17 +0200

    deploy

--- mathis.likesyou.org/htdocs/app/Views/troll.php
+++ mathis.likesyou.org/htdocs/app/Views/troll.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Invitation jsp</title>
+    <title>Invitation</title>
 
     <!-- Importation d'une police mignonne et arrondie -->
     <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@500;700&display=swap" rel="stylesheet">
