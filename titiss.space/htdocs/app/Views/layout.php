417aa7f776a9a16877a3d75a53d22b6990c6d7d3
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-07-10 21:39:54 +0200

    Update layout.php

--- titiss.space/htdocs/app/Views/layout.php
+++ titiss.space/htdocs/app/Views/layout.php
@@ -60,7 +60,14 @@
             <?php if (auth()->user()->inGroup('admin', 'superadmin')) { ?>
             <a href="<?php echo base_url('audit'); ?>" style="color: #80808099; font-size: small;">logs</a>
             <?php } ?>
-            <span class="welcome-text">👤 <?php echo esc(auth()->user()->username); ?></span>
+            <span class="welcome-text">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor"
+                    class="bi bi-person-circle" viewBox="0 0 16 16">
+                    <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                    <path fill-rule="evenodd"
+                        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                </svg>
+                <?php echo esc(auth()->user()->username); ?></span>
             <a href="<?php echo base_url('logout'); ?>" class="btn-logout">Déconnexion</a>
             <?php } else { ?>
             <a href="<?php echo base_url('login'); ?>" class="btn-login">Connexion</a>
