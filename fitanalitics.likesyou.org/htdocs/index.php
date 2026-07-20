7b2758b6b3112549588a0d928c0e0c7761c6f518
Titiss77 <Mathis.fcs.lav@gmail.com>
2026-07-20 22:31:17 +0200

    Update index.php

--- fitanalitics.likesyou.org/htdocs/index.php
+++ fitanalitics.likesyou.org/htdocs/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>FitAnalytics - Progression</title>
+    <title>FitAnalytics - Progression ptit test</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <style>
     :root {
